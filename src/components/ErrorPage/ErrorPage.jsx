09dd1ca7a3c69ccc998bb818096bfb5680e6107d
The Awesome <mohsindude5@gmail.com>
2023-09-26 14:53:45 +0600

    added / again

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="text-center">
-            <h2>404 Not Found!</h2>
-            <Link to='/'><button className="btn btn-primary">Back to Home</button></Link>
+        <div className="text-center pt-96">
+            <h2 className="text-6xl pb-8">404 Not Found!</h2>
+            <Link to='/'><button className="btn btn-outline btn-accent">Back to Home</button></Link>
         </div>
     );
 };
